cc24ab0be16e99e3cebeb3debeae3a0a01a57605
Antonio Rosales <antoniorosalesmartinez90@gmail.com>
2021-08-06 10:22:41 +0200

    FEAT styles basic

--- src/App.js
+++ src/App.js
@@ -6,13 +6,41 @@ const App = () => {
     <section className="timer-container">
       <section className="timer">
         <div>
-          <span className="mdi mdi-calendar-clock timer-icon">
-            <h2>Countdown Timer</h2>
+          <span className="mdi mdi-calendar-clock timer-icon"></span>
+          <h2>Countdown Timer</h2>
+          <p>
+            Countdown to a really special date. One you could or would never
+            imagine!
+          </p>
+        </div>
+        <div>
+          <section>
+            <p>87</p>
             <p>
-              Countdown to a really special date. One you could or would never
-              imagine!
+              <small>Days</small>
             </p>
-          </span>
+          </section>
+          <span>:</span>
+          <section>
+            <p>87</p>
+            <p>
+              <small>Hours</small>
+            </p>
+          </section>
+          <span>:</span>
+          <section>
+            <p>87</p>
+            <p>
+              <small>Minutes</small>
+            </p>
+          </section>
+          <span>:</span>
+          <section>
+            <p>87</p>
+            <p>
+              <small>Seconds</small>
+            </p>
+          </section>
         </div>
       </section>
     </section>
