b7375f5b73252ae5f95f3d561ca665681db9a960
Antonio Rosales <antoniorosalesmartinez90@gmail.com>
2021-08-06 11:29:39 +0200

    FIX styles, background and set {}

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 const App = () => {
@@ -9,20 +9,41 @@ const App = () => {
 
   var interval = useRef();
 
-  const startTuner = () => {
-    const countdownDate = new Date("May 30, 2020 00:00:00").getTime();
+  const startTimer = () => {
+    const countdownDate = new Date("September 17, 2021 00:00:00").getTime();
 
     interval = setInterval(() => {
       const now = new Date().getTime();
       const distance = countdownDate - now;
 
       const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      const hours = Math.floor(distance / (1000 * 60 * 60 * 24));
-      const minutes = Math.floor(distance / (1000 * 60 * 60 * 24));
-      const seconds = Math.floor(distance / (1000 * 60 * 60 * 24));
-    
+      const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (100 * 60 * 60)
+      );
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      if (distance < 0) {
+        // stop our timer
+        clearInterval(interval.current);
+      } else {
+        //update timer
+        setTimerDays(days);
+        setTimerHours(hours);
+        setTimerMinutes(minutes);
+        setTimerSeconds(seconds);
+      }
     }, 1000);
   };
+
+  // componentDidMount
+
+  useEffect(() => {
+    startTimer();
+    return () => {
+      clearInterval(interval.current);
+    };
+  });
 
   return (
     <section className="timer-container">
@@ -31,34 +52,33 @@ const App = () => {
           <span className="mdi mdi-calendar-clock timer-icon"></span>
           <h2>Countdown Timer</h2>
           <p>
-            Countdown to a really special date. One you could or would never
-            imagine!
+            DUNE
           </p>
         </div>
         <div>
           <section>
-            <p>87</p>
+            <p>{timerDays}</p>
             <p>
               <small>Days</small>
             </p>
           </section>
           <span>:</span>
           <section>
-            <p>87</p>
+            <p>{timerHours}</p>
             <p>
               <small>Hours</small>
             </p>
           </section>
           <span>:</span>
           <section>
-            <p>87</p>
+            <p>{timerMinutes}</p>
             <p>
               <small>Minutes</small>
             </p>
           </section>
           <span>:</span>
           <section>
-            <p>87</p>
+            <p>{timerSeconds}</p>
             <p>
               <small>Seconds</small>
             </p>
